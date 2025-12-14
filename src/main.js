26ddfcc3fcbd582f035594bd4fdede5bb72feb6a
Davei2023 <bogomazdavid@gmail.com>
2025-12-14 17:37:25 +0200

    Update main

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import './js/Animal-list/animal-list.js';
 import './js/Animal-details/animal-detalis.js';
 import './js/Succes/succes.js';
 import './js/Pets/pets.js';
-import './js/footer';
-import './js/Order-modal';
+import './js/footer/footer.js';
+import './js/Order-modal/order-modal.js';
 // import './js/';
