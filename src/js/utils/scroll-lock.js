0cf160954db521aeedc423d5577b499f589178a3
Anna Balesko <babasheldona@icloud.com>
2025-12-16 15:51:24 +0100

    add readme file

--- src/js/utils/scroll-lock.js
+++ src/js/utils/scroll-lock.js
@@ -5,3 +5,14 @@ export function lockScroll() {
 export function unlockScroll() {
   document.body.style.overflow = '';
 }
+
+export function smoothScrollTo(targetEl, offset = 0) {
+  if (!targetEl) return;
+
+  const top = targetEl.getBoundingClientRect().top + window.scrollY - offset;
+
+  window.scrollTo({
+    top,
+    behavior: 'smooth',
+  });
+}
